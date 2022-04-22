d5f0b6026a1d9b300f1b3575921bdd07393220b3
Sebas Lopez <sebastianlolpez@gmail.com>
2022-04-22 11:29:11 -0500

    Datos de curriculum
    se agregaron los atributos en la parte de actualizar datos

--- Back-end/src/routes/r_curriculum.js
+++ Back-end/src/routes/r_curriculum.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const userSchema = require("../models/m_curriculum");
-//insertar los datos correspondientes a la hoja de vida
-router.post("/users", (req, res) => {
+
+//C:insertar los datos correspondientes a la hoja de vida
+router.post("/m_curriculum", (req, res) => {
     const user = userSchema(req.body);
     user
         .save()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 })
+// R:consultar los datos de la hoja de vida
+router.get("/m_curriculum",  (req, res) => {
+    userSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => req.json({message: error}));
+    
+});
+
+
+// U: Actualizar datos de la hoja de vida
+router.put("/m_curriculum/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, direccion,telefono,correo, nacionalidad,perfilPro,formacion_academica } = req.body;
+    userSchema
+        .updateOne({ _id: id}, {
+            $set: {nombre, direccion,telefono,correo, nacionalidad,perfilPro,formacion_academica  }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => req.json({ message : error}));
+});
+
