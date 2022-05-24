d3f8465e2bb189ced1542bb1b9e5eebe3ccc9af9
garacevedo <gabrielandres.angel@gmail.com>
2022-05-24 16:56:01 -0500

    Periodo de estres para el login

--- Front-end/src/app/pages/login/login.component.ts
+++ Front-end/src/app/pages/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-login',
@@ -6,11 +11,49 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+  userFormLogin = this.formBuilder.group({
+    correo: '',
+    clave: ''
+  });
+  constructor(private userService: UserService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private _snackBar: MatSnackBar) {}
 
   ngOnInit() {
   }
   ngOnDestroy() {
   }
+/*
+  userLogin() {
+    if (this.userFormLogin.value['correo'] === ''||
+      this.userFormLogin.value['clave'] === '') {
+      this.openMessage("Falta informacion", "Cerrar");
+    } else {
+      this.userService.newUser(this.userFormLogin.value).subscribe(
+        () => {
+          //Redirigiendo a la ruta actual /User y recargando la ventana
+          //this.router.navigate(['/User']).then(() => {
+          //  window.location.reload();
+          //})
+          this.openMessage("User agregado", "Actualizar lista");
+        }
+
+      );
+    }
+  }
+
+  openMessage(message: string, action: string) {
+    let snackBarRef = this._snackBar.open(message, action);
+    if (message !== 'Falta información') {
+      snackBarRef.afterDismissed().subscribe(() => {
+        //Redirigiendo a la ruta actual /animal y recargando la ventana
+        this.router.navigate(['/animal']).then(() => {
+          window.location.reload();
+        });
+      });
+    }
+  }
+*/
 
 }
